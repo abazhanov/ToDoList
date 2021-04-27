d884b7210924f952fe2110abd085b29c419961d1
TRex-Dino <dmitrysinkov@gmail.com>
2021-04-27 18:46:40 +0300

    add basic element storyboard + controllers

---
+++ ToDoList/ViewControllers/ComplitedTaskViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ComplitedTaskViewController.swift
+//  ToDoList
+//
+//  Created by Dmitry on 27.04.2021.
+//
+
+import UIKit
+
+class ComplitedTaskViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
---
+++ ToDoList/ViewControllers/LogInViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LogInViewController.swift
+//  ToDoList
+//
+//  Created by Dmitry on 27.04.2021.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
